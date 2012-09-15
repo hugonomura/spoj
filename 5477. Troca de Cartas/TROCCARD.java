bfe18a2d2d3bc1a167f88b972c76267aea71adb5
Hugo Nomura <hugonomura@gmail.com>
2012-09-14 21:20:30 -0300

    adotando uma abordagem semelhante a hash

--- 5477. Troca de Cartas/TROCCARD.java
+++ 5477. Troca de Cartas/TROCCARD.java
@@ -4,47 +4,51 @@ import java.util.Arrays;
 class TROCCARD{
 	public static void main(String[] args) {
 		int qtdAline, qtdBeatriz;
-		int[] cartasAline;
-		int[] cartasBeatriz;
+		int[] cartasAline = new int[100000];
+		int[] cartasBeatriz = new int[100000];
+		int[] aline;
 		int qtdTrocaAline, qtdTrocaBeatriz;
 		Scanner entrada = new Scanner(System.in);
 
 		qtdAline = entrada.nextInt();
 		qtdBeatriz = entrada.nextInt();
 		while(qtdAline != 0 && qtdBeatriz != 0){
-			cartasAline = new int[qtdAline];
-			cartasBeatriz = new int[qtdBeatriz];
-			
-			cartasAline[0] = entrada.nextInt();
+			aline = new int[qtdAline];
+			qtdTrocaAline = 0;
+			qtdTrocaBeatriz = 0;
+
+			for(int i = 0; i < 100000; i++){
+				cartasAline[i] = -1;
+				cartasBeatriz[i] = -1;
+			}
+
+			aline[0] = entrada.nextInt() - 1;
+			cartasAline[aline[0]]++;
 			for(int i = 1; i < qtdAline; i++){
-				cartasAline[i] = entrada.nextInt();
-				if(cartasAline[i] == cartasAline[i-1]){
+				aline[i] = entrada.nextInt() - 1;
+				if(aline[i] == aline[i-1]){
 					i--;
 					qtdAline--;
-				}
-			}
-
-			cartasBeatriz[0] = entrada.nextInt();
-			// anterior = cartasBeatriz[0];
-			qtdTrocaBeatriz = 0;
-			if(Arrays.binarySearch(cartasAline, cartasBeatriz[0]) < 0){
-					qtdTrocaBeatriz++;
-			}
-			for(int i = 1; i < qtdBeatriz; i++){
-				cartasBeatriz[i] = entrada.nextInt();
-				if(cartasBeatriz[i] == cartasBeatriz[i-1]){
-					i--;
-					qtdBeatriz--;
 					continue;
 				}
-				if(Arrays.binarySearch(cartasAline, cartasBeatriz[i]) < 0){
-					qtdTrocaBeatriz++;
-				}
+				cartasAline[aline[i]]++;
 			}
 
-			qtdTrocaAline = 0;
+			int b = entrada.nextInt() - 1;
+			for(int i = 1; i < qtdBeatriz; i++){
+				int temp;
+				temp = entrada.nextInt() - 1;
+				if(cartasAline[temp] == -1 && temp != b){
+					qtdTrocaBeatriz++;
+					b = temp;
+					continue;
+				}
+				cartasBeatriz[temp]++;
+				b = temp;
+			}
+
 			for(int i = 0; i < qtdAline; i++){
-				if(Arrays.binarySearch(cartasBeatriz, cartasAline[i]) < 0){
+				if(cartasBeatriz[aline[i]] == -1){
 					qtdTrocaAline++;
 				}
 			}
@@ -54,8 +58,7 @@ class TROCCARD{
 			else
 				System.out.println(qtdTrocaBeatriz);
 
-			cartasAline = null;
-			cartasBeatriz = null;
+			aline = null;
 
 			qtdAline = entrada.nextInt();
 			qtdBeatriz = entrada.nextInt();
