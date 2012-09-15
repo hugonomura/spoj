86a6a91f3b33a1b9bd60f6cd4a812cdbac9591b1
Hugo Nomura <hugonomura@gmail.com>
2012-09-14 21:51:00 -0300

    otimizando a inicializacao das cartas

--- 5477. Troca de Cartas/TROCCARD.java
+++ 5477. Troca de Cartas/TROCCARD.java
@@ -7,20 +7,23 @@ class TROCCARD{
 		int[] cartasAline = new int[100000];
 		int[] cartasBeatriz = new int[100000];
 		int[] aline;
+		int[] beatriz;
 		int qtdTrocaAline, qtdTrocaBeatriz;
 		Scanner entrada = new Scanner(System.in);
 
-		qtdAline = entrada.nextInt();
-		qtdBeatriz = entrada.nextInt();
-		while(qtdAline != 0 && qtdBeatriz != 0){
-			aline = new int[qtdAline];
-			qtdTrocaAline = 0;
-			qtdTrocaBeatriz = 0;
-
-			for(int i = 0; i < 100000; i++){
+		for(int i = 0; i < 100000; i++){
 				cartasAline[i] = -1;
 				cartasBeatriz[i] = -1;
-			}
+		}
+
+		qtdAline = entrada.nextInt();
+		qtdBeatriz = entrada.nextInt();
+
+		while(qtdAline != 0 && qtdBeatriz != 0){
+			aline = new int[qtdAline];
+			beatriz = new int[qtdBeatriz];
+			qtdTrocaAline = 0;
+			qtdTrocaBeatriz = 0;
 
 			aline[0] = entrada.nextInt() - 1;
 			cartasAline[aline[0]]++;
@@ -34,17 +37,19 @@ class TROCCARD{
 				cartasAline[aline[i]]++;
 			}
 
-			int b = entrada.nextInt() - 1;
+			beatriz[0] = entrada.nextInt() - 1;
+			cartasBeatriz[beatriz[0]]++;
 			for(int i = 1; i < qtdBeatriz; i++){
-				int temp;
-				temp = entrada.nextInt() - 1;
-				if(cartasAline[temp] == -1 && temp != b){
-					qtdTrocaBeatriz++;
-					b = temp;
+				beatriz[i] = entrada.nextInt() - 1;
+				if(beatriz[i] == beatriz[i-1]){
+					i--;
+					qtdBeatriz--;
 					continue;
 				}
-				cartasBeatriz[temp]++;
-				b = temp;
+				if(cartasAline[beatriz[i]] == -1 && beatriz[i] != beatriz[i-1]){
+					qtdTrocaBeatriz++;
+				}
+				cartasBeatriz[beatriz[i]]++;
 			}
 
 			for(int i = 0; i < qtdAline; i++){
@@ -58,7 +63,16 @@ class TROCCARD{
 			else
 				System.out.println(qtdTrocaBeatriz);
 
+			for(int i = 0; i < qtdAline; i++){
+				cartasAline[aline[i]] = -1;
+			}
+
+			for(int i = 0; i < qtdBeatriz; i++){
+				cartasBeatriz[beatriz[i]] = -1;
+			}
+
 			aline = null;
+			beatriz = null;
 
 			qtdAline = entrada.nextInt();
 			qtdBeatriz = entrada.nextInt();
